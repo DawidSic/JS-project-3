3d450cf8d5dac229d26216436dc86656ed108de2
Dawid <dawidsicinski@gmail.com>
2023-09-30 00:48:24 +0200

    files

---
+++ js.js
@@ -0,0 +1,31 @@
+const pass = document.querySelector('#password')
+const p = document.querySelector('.passinfo')
+const letters = /[a-z]/i
+const numbers = /[0-9]/
+const special = /[!@#$%^&*()]/
+const minValue = 10
+
+
+const showMsg = () => {
+    if(pass.value.length > minValue && pass.value.match(letters) && pass.value.match(numbers) && pass.value.match(special)) {
+        p.textContent = 'Very good password'
+        p.style.color = 'lime'
+    } else if (pass.value.length > minValue && pass.value.match(letters) && pass.value.match(numbers)){
+        p.textContent = 'Good password'
+        p.style.color = 'gold'
+    } else {
+        p.textContent = 'Bad password'
+        p.style.color = 'tomato'
+    }
+}
+
+const checkPassword = () => {
+    if(pass.value !== ''){
+        showMsg()
+    } else {
+        p.textContent = 'No password...'
+        p.style.color = ''
+    }
+}
+
+pass.addEventListener('keyup', checkPassword)
